d0e65e6e8eb0e28b807771f40a9fb4f14bdd2eec
kurufeenve <kurufeenve@gmail.com>
2020-02-06 23:20:14 +0200

    test colors added

--- software/main.c
+++ software/main.c
@@ -3,15 +3,32 @@
 
 void	init_test_colors(t_color *test_colors)
 {
-	test_colors[0].color = 0x64AAC1;
-	test_colors[1].color = 0x1F81A7;
+	test_colors[0].color = 0x65AAC0;
+	test_colors[1].color = 0x2082A7;
+	test_colors[2].color = 0xB1B42A;
+	test_colors[3].color = 0x232D2B;
+	test_colors[4].color = 0x606D69;
+	test_colors[5].color = 0x6C91AF;
+	test_colors[6].color = 0x295B78;
+	test_colors[7].color = 0x243677;
+	test_colors[8].color = 0xCBC4B7;
+	test_colors[9].color = 0xC08C02;
+	test_colors[10].color = 0x942B25;
+	test_colors[11].color = 0x875638;
+	test_colors[12].color = 0x367C55;
+	test_colors[13].color = 0xF5F9F8;
+	test_colors[14].color = 0x615533;
+	test_colors[15].color = 0x8EB6A0;
+	test_colors[16].color = 0x585354;
+	test_colors[17].color = 0x96916B;
+	test_colors[18].color = 0x9D9F8F;
 }
 
 int		main(int argc, char** argv)
 {
 	t_headers	headers;
 	uint8_t		*file_data;
-	t_color		test_colors[8];
+	t_color		test_colors[18];
 
 	if (argc != 2)
 	{
