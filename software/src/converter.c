66abe8fb8db5725702a7d37ec64484139304319e
kurufeenve <kurufeenve@gmail.com>
2020-03-02 09:21:57 +0200

    trying to add make file

---
+++ software/src/converter.c
@@ -0,0 +1,52 @@
+#include "./includes/converter.h"
+
+void	print_bytes(uint8_t *bytes, size_t len)
+{
+	for(size_t i = 0; i < len; i++)
+	{
+		printf("%02x ", bytes[i]);
+		if (!(i % 16) && i != 0)
+		{
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
+
+void	put_pixel(uint8_t *data, t_color color, t_coords coords, t_headers headers)
+{
+	size_t	pixel_number;
+
+	if (headers.image_header.biBitCount == 32)
+	{
+		pixel_number = headers.file_header.bfOffBits + (coords.y * headers.image_header.biWidth + coords.x) * 4;
+		data[pixel_number + RESERVED] = color.channel[RESERVED];
+		data[pixel_number + RED] = color.channel[RED];
+		data[pixel_number + GREEN] = color.channel[GREEN];
+		data[pixel_number + BLUE] = color.channel[BLUE];
+	}
+	if (headers.image_header.biBitCount == 24) //does not work yet. problem in pixel_number calculation
+	{
+		pixel_number = headers.file_header.bfOffBits + (coords.y * headers.image_header.biWidth + coords.x) * 3;
+		data[pixel_number] = color.channel[RED];
+		data[pixel_number + 1] = color.channel[GREEN];
+		data[pixel_number + 2] = color.channel[BLUE];
+	}
+}
+
+void	image_processing(uint8_t *bytes, t_headers headers)
+{
+	t_coords	coords;
+	t_color		color;
+
+	color.color = 0xff55c5cf;
+	for (int i = 0; i < headers.image_header.biHeight; i++)
+	{
+		for (int j = 0; j < headers.image_header.biWidth; j++)
+		{
+			coords.x = j;
+			coords.y = i;
+			put_pixel(bytes, color, coords, headers);
+		}
+	}
+}
